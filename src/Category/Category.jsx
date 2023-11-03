3d3b3c462fb9908723a7014140876b78d4bd288f
Mahbubnishan <mahbubnishan6@gmail.com>
2023-11-03 18:27:07 +0600

    commite with category

--- src/Category/Category.jsx
+++ src/Category/Category.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import useProduct from '../Hooks/useProduct';
+import ProductCard from '../Pages/ProductCard/ProductCard';
 
 const Category = ({item}) => {
+    const [tabIndex, setTabIndex]=useState(0);
+    const [product]=useProduct();
+    const Pran = product.filter(item=>item.company==='PRAN');
+    const Nastle = product.filter(item=>item.company==='Nestle');
+    const Uniliver = product.filter(item=>item.company==='Uniliver');
+    const Marico= product.filter(item=>item.company==='Marico');
 
     return (
         <div>
-            <h3>{item.name}</h3>
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+            <TabList>
+                <Tab>Pran</Tab>
+                <Tab>Nastle</Tab>
+                <Tab>Uniliver</Tab>
+                <Tab>Marico</Tab>
+            </TabList>
+                <TabPanel>
+                    {
+                        Pran.map(item=><ProductCard
+                        item={item}
+                        ></ProductCard>)
+                    }
+                </TabPanel>
+                <TabPanel>
+                    
+                </TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,21 +4,13 @@ import useProduct from '../../Hooks/useProduct';
 import Category from '../../Category/Category';
 
 const Home = () => {
-    const [product]=useProduct();
-    const pran = product.filter(item=>item.company==='PRAN');
+    
 
     return (
         <div className=' m-10'>
-            <div>
-       {
-        pran.map(item=>
-        <Category 
-        item={item}>
-
-        </Category>)
-       }
-            </div>
+            
           <Slider></Slider>
+          <Category></Category>
         </div>
     );
 };
